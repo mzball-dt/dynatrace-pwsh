d55cb524c50c77b8090b686536e955a794bfd6f6
michael <michael.ball@dynatrace.com>
2020-05-06 11:44:34 +1000

    Update: invoke-USQLRequest - v1.2 Brought across to the new layout w/ the checks etc that that provides. Refac: README.md newline cleanup after an online edit

--- README.md
+++ README.md
@@ -3,52 +3,52 @@
 ... for making life easy.
 
 ### Invoke-USQLRequest.ps1
-    Powershell script for saving or viewing USQL queries as PS Tables or CSV files
-        Use case: 
-            - Extract USQL data from a tenant in a standard format
-        
+Powershell script for saving or viewing USQL queries as PS Tables or CSV files
+    Use case: 
+        - Extract USQL data from a tenant in a standard format
+    
 ### move-dashboard.ps1 (needs to be renamed copy-Dashboard)
-    Powershell script for moving Dynatrace Managed Dashboards between Managed clusters or tenants
-        Use case: 
-            - After creating a standard report, copying it to another tenant to share value
+Powershell script for moving Dynatrace Managed Dashboards between Managed clusters or tenants
+    Use case: 
+        - After creating a standard report, copying it to another tenant to share value
 
 ### assign-syntheticClusterLocation.ps1
-    Powershell script for assigning a Synthetic Location to a Dynatrace Managed Synthetic Cluster Node
-        Use case: 
-            - Providing a better system to add new cluster synthetic nodes - currently there's no UI
+Powershell script for assigning a Synthetic Location to a Dynatrace Managed Synthetic Cluster Node
+    Use case: 
+        - Providing a better system to add new cluster synthetic nodes - currently there's no UI
 
 ### get-tenantTokens.ps1
-    For auditing Tokens available in a given Dynatrace Tenant
+For auditing Tokens available in a given Dynatrace Tenant
 
 ### get-tenantTokenDetail.ps1
-    For when you only want information about a specific tenant token
-        Use case: 
-            - Checking scopes assigned to a token
-            - Checking scopes of tokens listed for use by run-clusterwide.ps1.
+For when you only want information about a specific tenant token
+    Use case: 
+        - Checking scopes assigned to a token
+        - Checking scopes of tokens listed for use by run-clusterwide.ps1.
 
 ### get-tenantDashboards.ps1
-    For auditing or reporting of who has created/shared dashboards in a given Dynatrace Tenant
+For auditing or reporting of who has created/shared dashboards in a given Dynatrace Tenant
 
 ### get-tenantHostGroupDetail.ps1
-    For reporting on all hosts in an environment, particularly for HU consumption by HostGroup
-        Use case: 
-            - What is the distribution of HU in my environment?
-            - What is the newest and oldest Agent version running in my environment?
-            - What Host Groups have the oldest agent version?
+For reporting on all hosts in an environment, particularly for HU consumption by HostGroup
+    Use case: 
+        - What is the distribution of HU in my environment?
+        - What is the newest and oldest Agent version running in my environment?
+        - What Host Groups have the oldest agent version?
 
 ### run-clusterWide.ps1
-    Powershell script for running arbitary code against a collection of Dynatrace tenants.
-        Use case: 
-            - Copying a new standard Dashboard to all tenants in a cluster or customer environment
-            - Exporting Config from all tenants in a cluster or customer environment
-            - Gathering Audit logs from all tenants in a cluster or customer environment
-            - Creating a new token for use by a data ingestion group across the customer environment
-            - Use any of the other scripts above across multiple tenants
+Powershell script for running arbitary code against a collection of Dynatrace tenants.
+    Use case: 
+        - Copying a new standard Dashboard to all tenants in a cluster or customer environment
+        - Exporting Config from all tenants in a cluster or customer environment
+        - Gathering Audit logs from all tenants in a cluster or customer environment
+        - Creating a new token for use by a data ingestion group across the customer environment
+        - Use any of the other scripts above across multiple tenants
 
 ### measure-HUperProperty.ps1
-    Pipeline script for performing simple (but irritating to type out) calculations on Host Meta-data
-        Use case: 
-            - How many HU in my tenant/cluster/grouping per OS/MonitoringType/environment
+Pipeline script for performing simple (but irritating to type out) calculations on Host Meta-data
+    Use case: 
+        - How many HU in my tenant/cluster/grouping per OS/MonitoringType/environment
 
 ## Goals
 
--- invoke-USQLRequest.ps1
+++ invoke-USQLRequest.ps1
@@ -1,24 +1,26 @@
 <#
     .Synopsis
-        Powershell script for viewing or saving Dynatrace USQL queries as Powershell tables and csv files
+        For viewing or saving Dynatrace USQL queries as Powershell tables and csv files
 
     .Description
         There's currently no well-supported way to export USQL table to file. This is a problem this simple script resolves.
 
-        There is currently no checking done on inputs. Hopefully the Dynatrace server API will provide enough of an error.
-
     .Notes
         Author: Michael Ball
-        Version: 1.1.0
+        Version: 1.2.0 - 20200506
 
         ChangeLog
-            1.0.0 
-                MVP - Things work
+            1.2.0
+                Updated with new standard scaffolding
+                Implicitly loaded System.web prior to urlencode to hopefully prevent any issues with that being missing
             1.1.0
                 - Added -periodType and timePeriod to specify time for reported data. 
                 - Added pageSize to map to the dt api pagesize var
                 - Minimal cleanup/checking of dtenv input (they come in all shapes and sizes)
                 - Removed the return as a format-table -- this was a bad idea, users should be the one parsing the output to format-table
+            1.0.0 
+                MVP - Things work
+            
 
     .Example
         ./invoke-usqlRequest.ps1 -dtenv id.live.dynatrace.com -token $(get-content token.file) -USQL 'select city from usersession'
@@ -34,37 +36,69 @@
         ...
 #>
 
-param(
-    # The Dynatrace environment to query - this is the 'homepage' of the target eg. https://lasjdh3.live.dynatrace.com/ or https://dynatrace-managed.com/e/UUID
-    [Parameter(Mandatory=$true)][String]$dtenv,
-    # The token to query the environment with - much have User Session access
-    [Parameter(Mandatory=$true)][String]$token,
+PARAM (
+    # The cluster or tenant the HU report should be fetched from
+    [Parameter()][ValidateNotNullOrEmpty()] $dtenv = $env:dtenv,
+    # Token must have Smartscape Read access for a tenant
+    [Alias('dttoken')][ValidateNotNullOrEmpty()][string] $token = $env:dttoken,
+
     # The USQL Query to run against the cluster
-    [Parameter(Mandatory=$true)][String]$USQL,
+    [ValidateNotNullOrEmpty()][String]$USQL,
     # What period length should be reported
-    [ValidateSet('minute','hour','day', 'week')][string]$periodType = 'hour',
+    [ValidateSet('minute', 'hour', 'day', 'week')][string]$periodType = 'hour',
     # How many of these time periods back should be requested
     [String]$timePeriod = 2,
     # Number of results to pull back
     [int]$pageSize = 500,
-    
-    # Export the collected USQL to csv
-    [String]$OutFile
+    # Path to output a csv representation of the fetched data.
+    [string] $outfile,
+
+    # Prints Help output
+    [Alias('h')][switch] $help,
+    # use this switch to tell this script to not check token or cluster viability
+    [switch] $noCheckCompatibility,
+    # use this switch to tell powershell to ignore ssl concerns
+    [switch] $noCheckCertificate,
+
+    # DO NOT USE - This is set by Script Author
+    [String[]]$script:tokenPermissionRequirements = @('DTAQLAccess', 'DataExport')
 )
 
-function convertTo-jsDate($date) {
-    return [Math]::Floor(1000 * (Get-Date ($date) -UFormat %s))
+# Help flag checks
+if ($h -or $help) {
+    Get-Help $script:MyInvocation.MyCommand.Path -Detailed
+    exit 0
 }
 
-$timePeriodHash = @{
-    minute = 1000*60;
-    hour = 1000*60*60;
-    day = 1000*60*60*24;
-    week = 1000*60*60*24*7;
+# Ensure that dtenv and token are both populated
+if (!$script:dtenv) {
+    return Write-Error "dtenv was not populated - unable to continue"
+}
+elseif (!$script:token) {
+    return Write-Error "token/dttoken was not populated - unable to continue"
+}
+elseif (!$script:USQL) {
+    return Write-Error "USQL argument was not populated - unable to continue"
 }
 
-# custom policy required required for old pwsh versions and bad SSLs
-if ($PSVersionTable.PSVersion.Major -lt 6) {
+# Try to 'fix' a missing https:// in the env
+if ($script:dtenv -notlike "https://*" -and $script:dtenv -notlike "http://*") {
+    Write-Host -ForegroundColor DarkYellow -Object "WARN: Environment URI was missing 'httpx://' prefix"
+    $script:dtenv = "https://$script:dtenv"
+    Write-host -ForegroundColor Cyan "New environment URL: $script:dtenv"
+}
+
+# Try to 'fix' a trailing '/'
+if ($script:dtenv[$script:dtenv.Length - 1] -eq '/') { 
+    $script:dtenv = $script:dtenv.Substring(0, $script:dtenv.Length - 1) 
+    write-host -ForegroundColor DarkYellow -Object "WARNING: Removed trailing '/' from dtenv input"
+}
+
+$baseURL = "$script:dtenv/api/v1"
+
+# Setup Network settings to work from less new setups
+if ($nocheckcertificate) {
+    # SSL and other compatability settings
     function Disable-SslVerification {
         if (-not ([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
             Add-Type -TypeDefinition  @"
@@ -73,43 +107,94 @@ using System.Security.Cryptography.X509Certificates;
 public static class TrustEverything
 {
     private static bool ValidationCallback(object sender, X509Certificate certificate, X509Chain chain,
-        SslPolicyErrors sslPolicyErrors) { return true; }
+    SslPolicyErrors sslPolicyErrors) { return true; }
     public static void SetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = ValidationCallback; }
-    public static void UnsetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = null; }
-}
+    public static void UnsetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = null; } } 
 "@
         }
         [TrustEverything]::SetCallback()
     }
-    
     function Enable-SslVerification {
         if (([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
             [TrustEverything]::UnsetCallback()
         }
     }
-     
-    Disable-SslVerification    
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocoltype]::Tls12
+    Disable-SslVerification   
 }
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocoltype]::Tls12 
 
-# Construct the headers for this API request
+# Construct the headers for this API request 
 $headers = @{
-    Authorization  = "Api-Token $script:token"
-    "Content-Type" = "application/json"
+    Authorization  = "Api-Token $script:token";
+    Accept         = "application/json; charset=utf-8";
+    "Content-Type" = "application/json; charset=utf-8"
 }
 
-# Do some clean up on baseURL and then prep for the API call
-$baseURL = $script:dtenv
-if ($baseURL[$baseURL.Length-1] -eq '/') {$baseURL = $baseURL.Substring(0, $baseURL.Length-2)}
-if ($baseURL -notmatch '^https?://') {$baseURL = "https://$baseURL"}
-$baseURL = "$baseURL/api/v1/userSessionQueryLanguage/table"
+if (!$noCheckCompatibility) {
+    <#
+        Determine what type environment we have? This script will only work on tenants 
+        
+        SaaS tenant = https://*.live.dynatrace.com
+        Managed tenant = https://*/e/UUID
+        Managed Cluster = https://*
+    #>
+    $envType = 'cluster'
+    if ($script:dtenv -like "*.live.dynatrace.com") {
+        $envType = 'env'
+    }
+    elseif ($script:dtenv -like "http*://*/e/*") {
+        $envType = 'env'
+    }
+
+    # Script won't work on a cluster
+    if ($envType -eq 'cluster') {
+        write-error "'$script:dtenv' looks like an invalid URL (and Clusters are not supported by this script)"
+        return
+    }
+    
+    # Environment version check - cancel out if too old 
+    $uri = "$baseURL/config/clusterversion"
+    Write-Host -ForegroundColor cyan -Object "Cluster Version Check: $uri"
+    $res = Invoke-RestMethod -Method GET -Headers $headers -Uri $uri 
+    $envVersion = $res.version -split '\.'
+    if ($envVersion -and ([int]$envVersion[0]) -ne 1 -and ([int]$envVersion[1]) -lt 176) {
+        write-host "Failed Environment version check - Expected: > 1.176 - Got: $($res.version)"
+        return
+    }
+
+    # Token has required Perms Check - cancel out if it doesn't have what's required
+    $uri = "$baseURL/tokens/lookup"
+    Write-Host -ForegroundColor cyan -Object "Token Permissions Check: $uri"
+    $res = Invoke-RestMethod -Method POST -Headers $headers -Uri $uri -body "{ `"token`": `"$script:token`"}"
+    if (($script:tokenPermissionRequirements | Where-Object { $_ -notin $res.scopes }).count) {
+        write-host "Failed Token Permission check. Token requires: $($script:tokenPermissionRequirements -join ',')"
+        write-host "Token provided only had: $($res.scopes -join ',')"
+        return
+    }
+}
+
+function convertTo-jsDate($date) {
+    return [Math]::Floor(1000 * (Get-Date ($date) -UFormat %s))
+}
+
+$timePeriodHash = @{
+    minute = 1000*60;
+    hour   = 1000*60*60;
+    day    = 1000*60*60*24;
+    week   = 1000*60*60*24*7;
+}
+
+$baseURL = "$baseURL/userSessionQueryLanguage/table"
 
 # Add the time and other params
 $now = convertTo-jsDate ([datetime]::UtcNow)
 $start = $now - $timePeriodHash[$script:periodType] * $script:timePeriod
 
 $baseURL = $baseURL + "?startTimestamp=$start&endTimestamp=$now&pageSize=$script:pageSize"
- 
+
+# Add the System.Web type - the lack of this will be a headache otherwise.
+Add-Type -AssemblyName System.Web -ErrorAction SilentlyContinue | Out-Null
+
 # Encode the query (required by the call)
 $query = [System.Web.HttpUtility]::UrlEncode($script:USQL) -replace '\+', '%20'
  
@@ -118,11 +203,7 @@ $uri = "$baseURL`&query=$query"
 write-host $uri -ForegroundColor Cyan
 
 # make the call, being aware of different pwsh versions
-if ($PSVersionTable.PSVersion.Major -lt 6) {
-    $response = Invoke-RestMethod -Method GET -Uri $uri -Headers $headers
-} else {
-    $response = Invoke-RestMethod -Method GET  -Uri $uri -Headers $headers -skipcertificatecheck
-}
+$response = Invoke-RestMethod -Method GET -Uri $uri -Headers $headers
 
 # iterate through the returned data to construct a PWSH Object, output and possibly save it
 $data = @()
@@ -138,7 +219,7 @@ Foreach ($row in $response.values) {
 
 # If we need to output to file
 if ($script:outfile) {
-    if (!(Split-Path $script:outfile| Test-Path -PathType Container)) {
+    if (!(Split-Path $script:outfile | Test-Path -PathType Container)) {
         New-Item -Path (Split-Path $script:outfile) -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
     }
     $outputFile = New-Item -Path $script:outfile -Force
