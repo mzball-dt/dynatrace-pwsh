58cf761967a8ec1496c44c8be1abdedbdc0523e9
michael <michael.ball@dynatrace.com>
2020-05-13 17:45:04 +1000

    update: v1.1.0 get-tenantDashboards Updated to match the scaffold we're using everywhere

--- get-tenantDashboards.ps1
+++ get-tenantDashboards.ps1
@@ -12,9 +12,11 @@
 
 .Notes
     Author: Michael Ball
-    Version: 1.0.0 - 20200323
+    Version: 1.1.0 - 20200513
 
     ChangeLog
+        1.1.0
+            Updated script to work with the new scaffolding
         1.0.0
             MVP - Things work - basic script
         
@@ -22,25 +24,62 @@
     ./get-tenantDashboards.ps1 -dtenv "https://lasjdh3.live.dynatrace.com/" -token 'asdfu12312938' | format-table
 #>
 
-param(
-    # The Dynatrace environment to query - this is the 'homepage' of the target eg. https://lasjdh3.live.dynatrace.com/ or https://dynatrace-managed.com/e/UUID
-    [Parameter(Mandatory = $true)][String]$dtenv,
-    # The token to query the environment with - must have environment token config access
-    [Parameter(Mandatory = $true)][String]$token,
+[CmdletBinding(DefaultParametersetName="default")]
+PARAM (
+    # The cluster or tenant the HU report should be fetched from
+    [Parameter()][ValidateNotNullOrEmpty()] $dtenv = $env:dtenv,
+    # Token must have Smartscape Read access for a tenant
+    [Alias('dttoken')][ValidateNotNullOrEmpty()][string] $token = $env:dttoken,
 
     # Show a subset of information collected
-    [switch]$short,
+    [Alias('summary')][switch]$short,
+    # Path to output a csv representation of the fetched data.
+    [string] $outfile,
     
-    # Export the collected USQL to csv
-    [String]$OutFile
+    # Prints Help output
+    [Alias('h')][switch] $help,
+    # use this switch to tell this script to not check token or cluster viability
+    [switch] $noCheckCompatibility,
+    # use this switch to tell powershell to ignore ssl concerns
+    [switch] $noCheckCertificate,
+
+    # DO NOT USE - This is set by Script Author
+    [String[]]$script:tokenPermissionRequirements = @('DataExport','ReadConfig')
 )
 
-function convertTo-jsDate($date) {
-    return [Math]::Floor(1000 * (Get-Date ($date) -UFormat %s))
+# Help flag checks
+if ($h -or $help) {
+    Get-Help $script:MyInvocation.MyCommand.Path -Detailed
+    exit 0
 }
 
-# custom policy required required for old pwsh versions and bad SSLs
-if ($PSVersionTable.PSVersion.Major -lt 6) {
+# Ensure that dtenv and token are both populated
+if (!$script:dtenv) {
+    return Write-Error "dtenv was not populated - unable to continue"
+    
+} elseif (!$script:token) {
+    return Write-Error "token/dttoken was not populated - unable to continue"
+    
+}
+
+# Try to 'fix' a missing https:// in the env
+if ($script:dtenv -notlike "https://*" -and $script:dtenv -notlike "http://*") {
+    Write-Host -ForegroundColor DarkYellow -Object "WARN: Environment URI was missing 'httpx://' prefix"
+    $script:dtenv = "https://$script:dtenv"
+    Write-host -ForegroundColor Cyan "New environment URL: $script:dtenv"
+}
+
+# Try to 'fix' a trailing '/'
+if ($script:dtenv[$script:dtenv.Length - 1] -eq '/') { 
+    $script:dtenv = $script:dtenv.Substring(0, $script:dtenv.Length - 1) 
+    write-host -ForegroundColor DarkYellow -Object "WARNING: Removed trailing '/' from dtenv input"
+}
+
+$baseURL = "$script:dtenv/api/v1"
+
+# Setup Network settings to work from less new setups
+if ($nocheckcertificate) {
+    # SSL and other compatability settings
     function Disable-SslVerification {
         if (-not ([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
             Add-Type -TypeDefinition  @"
@@ -49,54 +88,93 @@ using System.Security.Cryptography.X509Certificates;
 public static class TrustEverything
 {
     private static bool ValidationCallback(object sender, X509Certificate certificate, X509Chain chain,
-        SslPolicyErrors sslPolicyErrors) { return true; }
+    SslPolicyErrors sslPolicyErrors) { return true; }
     public static void SetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = ValidationCallback; }
-    public static void UnsetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = null; }
-}
+    public static void UnsetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = null; } } 
 "@
         }
         [TrustEverything]::SetCallback()
     }
-    
     function Enable-SslVerification {
         if (([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
             [TrustEverything]::UnsetCallback()
         }
     }
-     
-    Disable-SslVerification    
-    [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocoltype]::Tls12
+    Disable-SslVerification   
 }
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocoltype]::Tls12 
 
-# Construct the headers for this API request
+# Construct the headers for this API request 
 $headers = @{
-    Authorization  = "Api-Token $script:token"
-    "Content-Type" = "application/json"
+    Authorization  = "Api-Token $script:token";
+    Accept         = "application/json; charset=utf-8";
+    "Content-Type" = "application/json; charset=utf-8"
 }
 
-# Do some clean up on baseURL and then prep for the API call
-$baseURL = $script:dtenv
-if ($baseURL[$baseURL.Length - 1] -eq '/') { $baseURL = $baseURL.Substring(0, $baseURL.Length - 2) }
-if ($baseURL -notmatch '^https?://') { $baseURL = "https://$baseURL" }
-$baseURL = "$baseURL/api/config/v1/dashboards"
+if (!$noCheckCompatibility) {
+    <#
+        Determine what type environment we have? This script will only work on tenants 
+        
+        SaaS tenant = https://*.live.dynatrace.com
+        Managed tenant = https://*/e/UUID
+        Managed Cluster = https://*
+    #>
+    $envType = 'cluster'
+    if ($script:dtenv -like "*.live.dynatrace.com") {
+        $envType = 'env'
+    } elseif ($script:dtenv -like "http*://*/e/*") {
+        $envType = 'env'
+    }
 
-write-host -ForegroundColor cyan $baseURL
+    # Script won't work on a cluster
+    if ($envType -eq 'cluster') {
+        write-error "'$script:dtenv' looks like an invalid URL (and Clusters are not supported by this script)"
+        return
+    }
+    
+    # Environment version check - cancel out if too old 
+    $uri = "$baseURL/config/clusterversion"
+    Write-Host -ForegroundColor cyan -Object "Cluster Version Check: $uri"
+    $res = Invoke-RestMethod -Method GET -Headers $headers -Uri $uri 
+    $envVersion = $res.version -split '\.'
+    if ($envVersion -and ([int]$envVersion[0]) -ne 1 -and ([int]$envVersion[1]) -lt 176) {
+        write-host "Failed Environment version check - Expected: > 1.176 - Got: $($res.version)"
+        return
+    }
+
+    # Token has required Perms Check - cancel out if it doesn't have what's required
+    $uri = "$baseURL/tokens/lookup"
+    Write-Host -ForegroundColor cyan -Object "Token Permissions Check: $uri"
+    $res = Invoke-RestMethod -Method POST -Headers $headers -Uri $uri -body "{ `"token`": `"$script:token`"}"
+    if (($script:tokenPermissionRequirements | Where-Object {$_ -notin $res.scopes}).count) {
+        write-host "Failed Token Permission check. Token requires: $($script:tokenPermissionRequirements -join ',')"
+        write-host "Token provided only had: $($res.scopes -join ',')"
+        return
+    }
+}
+function convertTo-jsDate($date) {
+    return [Math]::Floor(1000 * (Get-Date ($date) -UFormat %s))
+}
+
+$uri = "$script:dtenv/api/config/v1/dashboards"
+Write-host -ForegroundColor cyan "Get list of Dashboards: $uri"
 if ($PSVersionTable.PSVersion.Major -lt 6) {
-    $response = Invoke-RestMethod -Method GET -Uri $baseURL -Headers $headers
+    $response = Invoke-RestMethod -Method GET -Uri $uri -Headers $headers
 }
 else {
-    $response = Invoke-RestMethod -Method GET -Uri $baseURL -Headers $headers -skipcertificatecheck
+    $response = Invoke-RestMethod -Method GET -Uri $uri -Headers $headers -skipcertificatecheck
 }
 
 # iterate through the returned data to construct a PWSH Object, output and possibly save it
 $data = @()
 Foreach ($row in $response.dashboards) {
     if ($PSVersionTable.PSVersion.Major -lt 6) {
-        $completeDashboard = Invoke-RestMethod -Method GET -Uri "$baseURL/$($row.id)" -Headers $headers
+        $completeDashboard = Invoke-RestMethod -Method GET -Uri "$uri/$($row.id)" -Headers $headers
     }
     else {
-        $completeDashboard = Invoke-RestMethod -Method GET -Uri "$baseURL/$($row.id)" -Headers $headers -skipcertificatecheck
+        $completeDashboard = Invoke-RestMethod -Method GET -Uri "$uri/$($row.id)" -Headers $headers -skipcertificatecheck
     }
+
 
     $dashboardSummary = New-Object psobject
     $dashboardSummary | Add-Member -MemberType NoteProperty -Name "Owner" -Value $completeDashboard.dashboardMetadata.owner
@@ -111,6 +189,7 @@ Foreach ($row in $response.dashboards) {
     $dashboardSummary | Add-Member -MemberType NoteProperty -Name "id" -Value $completeDashboard.id
 
     $data += $dashboardSummary
+    # $rawdata += $completeDashboard
 }
 
 # If we need to output to file
