c2bac04c54d53352f2e20218e65f658984616985
michael <michael.ball@dynatrace.com>
2020-07-23 13:51:53 +1000

    added get-clusterUser WIP script It's only really missing examples and last do-over

---
+++ wipScripts/get-clusterUser.ps1
@@ -0,0 +1,177 @@
+<#
+.SYNOPSIS
+    Get information about users of a specific Dynatrace Managed Cluster
+
+.DESCRIPTION
+    Returns all the information available for the users of a dynatrace cluster.
+
+.NOTES
+    Version: alpha - 20200713
+    Author: michael.ball8
+    Requirements: Powershell 5+
+    ChangeLog: 
+    alpha
+        -     
+#>
+
+<###########################
+# Start of scaffold block #
+###########################>
+
+PARAM (
+    # The cluster or tenant the Dasboard will be placed in
+    [Parameter()][ValidateNotNullOrEmpty()] $dtcluster = $env:dtcluster,
+    # Token for the destination tenant w/ DataExport and WriteConfig perms
+    [Alias('dtclustertoken')][ValidateNotNullOrEmpty()][string] $clustertoken = $env:dtclustertoken,
+
+    <##################################
+    # Start of Script-specific params #
+    ##################################>
+
+    <#################################
+    # Stop of Script-specific params #
+    #################################>
+
+    # Prints Help output
+    [Alias('h')][switch] $help,
+    # use this switch to tell this script to not check token or cluster viability
+    [switch] $noCheckCompatibility,
+    # use this switch to tell powershell to ignore ssl concerns
+    [switch] $noCheckCertificate,
+
+    # DO NOT USE - This is set by Script Author
+    [String[]]$script:tokenPermissionRequirements = @('ServiceProviderAPI')
+)
+
+# Help flag checks
+if ($h -or $help) {
+    Get-Help $script:MyInvocation.MyCommand.Path -Detailed
+    exit 0
+}
+
+# Ensure that dtenv and token are both populated
+if (!$script:dtcluster) {
+    return Write-Error "dtcluster was not populated - unable to continue"
+}
+elseif (!$script:clustertoken) {
+    return Write-Error "clustertoken/dtclustertoken was not populated - unable to continue"
+}
+
+# Try to 'fix' a missing https:// in the env
+if ($script:dtcluster -notlike "https://*" -and $script:dtcluster -notlike "http://*") {
+    Write-Host -ForegroundColor DarkYellow -Object "WARN: Environment URI was missing 'httpx://' prefix"
+    $script:dtcluster = "https://$script:dtcluster"
+    Write-host -ForegroundColor Cyan "New environment URL: $script:dtcluster"
+}
+
+# Try to 'fix' a trailing '/'
+if ($script:dtcluster[$script:dtcluster.Length - 1] -eq '/') { 
+    $script:dtcluster = $script:dtcluster.Substring(0, $script:dtcluster.Length - 1) 
+    write-host -ForegroundColor DarkYellow -Object "WARNING: Removed trailing '/' from dtenv input"
+}
+
+$baseURL = "$script:dtcluster/api/cluster/v1"
+
+# Setup Network settings to work from less new setups
+if ($nocheckcertificate) {
+    # SSL and other compatability settings
+    function Disable-SslVerification {
+        if (-not ([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
+            Add-Type -TypeDefinition  @"
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+public static class TrustEverything
+{
+    private static bool ValidationCallback(object sender, X509Certificate certificate, X509Chain chain,
+    SslPolicyErrors sslPolicyErrors) { return true; }
+    public static void SetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = ValidationCallback; }
+    public static void UnsetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = null; } } 
+"@
+        }
+        [TrustEverything]::SetCallback()
+    }
+    function Enable-SslVerification {
+        if (([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
+            [TrustEverything]::UnsetCallback()
+        }
+    }
+    Disable-SslVerification   
+}
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocoltype]::Tls12 
+
+# Construct the headers for this API request 
+$headers = @{
+    Authorization  = "Api-Token $script:clustertoken";
+    Accept         = "application/json; charset=utf-8";
+    "Content-Type" = "application/json; charset=utf-8"
+}
+
+function confirm-supportedClusterVersion ($minimumVersion = 176, $logmsg = '') {
+    # Environment version check - cancel out if too old 
+    $uri = "$script:dtcluster/api/v1.0/onpremise/cluster"
+    Write-Host -ForegroundColor cyan -Object "Cluster Version Check$logmsg`: GET $uri"
+    $res = Invoke-RestMethod -Method GET -Headers $headers -Uri $uri 
+    $envVersion = $res[0].buildVersion -split '\.'
+    if ($envVersion -and ([int]$envVersion[0]) -ne 1 -and ([int]$envVersion[1]) -lt $minimumVersion) {
+        write-host "Failed Environment version check - Expected: > 1.176 - Got: $($res.version)"
+        exit
+    }
+}
+
+function confirm-requireTokenPerms ($token, $requirePerms, $logmsg = '') {
+    # Token has required Perms Check - cancel out if it doesn't have what's required
+    $uri = "$baseURL/tokens/lookup"
+    Write-Host -ForegroundColor cyan -Object "Token Permissions Check$logmsg`: POST $uri"
+    $res = Invoke-RestMethod -Method POST -Headers $headers -Uri $uri -body "{ `"token`": `"$script:clustertoken`"}"
+    if (($requirePerms | Where-Object { $_ -notin $res.scopes }).count) {
+        write-host "Failed Token Permission check. Token requires: $($requirePerms -join ',')"
+        write-host "Token provided only had: $($res.scopes -join ',')"
+        exit
+    }
+}
+
+if (!$noCheckCompatibility) {
+    <#
+        Determine what type environment we have? This script will only work on tenants 
+        
+        SaaS tenant = https://*.live.dynatrace.com
+        Managed tenant = https://*/e/UUID
+        Managed Cluster = https://*
+    #>
+    $envType = 'cluster'
+    if ($script:dtcluster -like "*.live.dynatrace.com") {
+        $envType = 'env'
+    }
+    elseif ($script:dtcluster -like "http*://*/e/*") {
+        $envType = 'env'
+    }
+
+    # Script won't work on a tenant
+    if ($envType -ne 'cluster') {
+        write-error "'$script:dtcluster' looks like an invalid URL (only Clusters are supported by this script)"
+        return
+    }
+    
+    confirm-supportedClusterVersion 184
+    confirm-requireTokenPerms $script:clustertoken $script:tokenPermissionRequirements
+}
+
+<#########################
+# Stop of scaffold block #
+#########################>
+
+
+# Handles the fetching - used mostly by the cluster wide checks 
+$headers = @{
+    "Authorization"  = "Api-Token $clustertoken"
+    "Content-Type" = "application/json"
+}
+$baseURL = "$dtcluster/api/v1.0"
+
+# create a baseURL for the API call
+$uri = "$baseURL/onpremise/users"
+write-host -ForegroundColor cyan "Host Entity information request: $uri"
+$res = Invoke-RestMethod -Method GET -Headers $headers -Uri $uri
+$res = $res | Select-Object -Property 'id','email','firstName','lastName','groups'
+
+return $res
