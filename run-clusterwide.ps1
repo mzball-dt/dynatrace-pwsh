40cd3496656fabfed6e6a381c494058c311df98b
michael <michael.ball@dynatrace.com>
2020-11-09 14:27:03 +1100

    run-clusterwide bumped to v2.0.1

--- run-clusterwide.ps1
+++ run-clusterwide.ps1
@@ -26,10 +26,20 @@
 
 .NOTES
     Author: michael.ball
-    Version: 1.0.0 - 20200504
+    Version: 2.0.1 - 20201106
     Requirement: Powershell v5.0
 
     Changelog:  
+        2.0.1
+            Update to fix broken -tenantid functionality. This will now work as expected.
+        2.0.0
+            By default 'discovers' and executes $Scriptblock against all tenants in the cluster
+            Target tenants can be filtered using $tenantId/$filter/$tag parameters
+            Creates and deletes a Environment Token Management Token during execution for token creation in tenants
+
+
+        1.1.0
+            Updated to use the Cluster v2 API
         1.0.0
             Don't know why this wasn't v1? 
             Now also passes through the name provided in the CSV structure for better naming reasons
@@ -47,74 +57,289 @@
 
 #>
 
+<###########################
+# Start of scaffold block #
+###########################>
+
 PARAM (
+    # The cluster or tenant the Dasboard will be placed in
+    [Parameter()][ValidateNotNullOrEmpty()] $dtcluster = $env:dtcluster,
+    # Token for the destination tenant w/ DataExport and WriteConfig perms
+    [Alias('dtclustertoken')][ValidateNotNullOrEmpty()][string] $clustertoken = $env:dtclustertoken,
+
+    <##################################
+    # Start of Script-specific params #
+    ##################################>
+
     # A specific tenant (by name) from the list of known tenants
-    [String] $tenant,
-    # A specific tenant (by index) from the list of known tenants
-    [int] $tenantIndex,
+    [ValidateNotNullOrEmpty()][String] $tenantid,
+    # An 'include' filter applied to the tenant names in a cluster
+    [ValidateNotNullOrEmpty()][String] $filter,
+    # A list of tags must all be present for a tenant to be included
+    [ValidateNotNullOrEmpty()][String[]] $tag,
+
     # The Script block to execute for each tenant
     [Parameter(ValueFromPipeline = $true)][scriptblock] $ScriptBlock,
-    
-    # External CSV list of Dynatrace Tenants
-    [String] $tenantsCSVFile,
+
+    # required Token perms
+    [ValidateNotNullOrEmpty()][String[]] $envtokenPerms = @("DataExport", "ReadConfig", "WriteConfig", "DTAQLAccess", "TenantTokenManagement", "ReadSyntheticData", "ReadAuditLogs", "entities.read", "networkZones.read", "activeGates.read"),
+    # The token expiry time in seconds for any tokens we can't delete
+    [ValidateNotNullOrEmpty()][String[]] $envTokenActiveTime = 60,
 
     # Use this switch to list the internal tenants (w/o tokens)
     [switch] $list,
 
     # Maximum Concurrent Jobs managed by this script
-    [int] $maxJobs = 8, 
+    [int] $maxJobs = 8,
     # Time between each loop of creating and checking jobs
     [int] $JobDelay = 100,
 
-    # List detailed help
-    [switch] $help,    
+    <#################################
+    # Stop of Script-specific params #
+    #################################>
+
+    # Prints Help output
+    [Alias('h')][switch] $help,
+    # use this switch to tell this script to not check token or cluster viability
+    [switch] $noCheckCompatibility,
     # use this switch to tell powershell to ignore ssl concerns
-    [switch] $nocheckcertificate
+    [switch] $noCheckCertificate,
+
+    # DO NOT USE - This is set by Script Author
+    [String[]]$script:tokenPermissionRequirements = @('ServiceProviderAPI', 'ClusterTokenManagement')
 )
 
 # Help flag checks
 if ($h -or $help) {
-    Get-Help $(join-path $(Get-Location) $MyInvocation.MyCommand.Name) -Detailed
+    Get-Help $script:MyInvocation.MyCommand.Path -Detailed
     exit 0
 }
 
+# Ensure that dtenv and token are both populated
+if (!$script:dtcluster) {
+    return Write-Error "dtcluster was not populated - unable to continue"
+}
+elseif (!$script:clustertoken) {
+    return Write-Error "clustertoken/dtclustertoken was not populated - unable to continue"
+}
 
-# A csv styled list of environment URLs w/ tokens that have base permissions
-$completeTenantList_text = @"
-Name,Environment,token
+# Try to 'fix' a missing https:// in the env
+if ($script:dtcluster -notlike "https://*" -and $script:dtcluster -notlike "http://*") {
+    Write-Host -ForegroundColor DarkYellow -Object "WARN: Environment URI was missing 'httpx://' prefix"
+    $script:dtcluster = "https://$script:dtcluster"
+    Write-host -ForegroundColor Cyan "New environment URL: $script:dtcluster"
+}
+
+# Try to 'fix' a trailing '/'
+if ($script:dtcluster[$script:dtcluster.Length - 1] -eq '/') { 
+    $script:dtcluster = $script:dtcluster.Substring(0, $script:dtcluster.Length - 1) 
+    write-host -ForegroundColor DarkYellow -Object "WARNING: Removed trailing '/' from dtenv input"
+}
+
+$baseURL = "$script:dtcluster/api/cluster/v1"
+
+# Setup Network settings to work from less new setups
+if ($nocheckcertificate) {
+    # SSL and other compatability settings
+    function Disable-SslVerification {
+        if (-not ([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
+            Add-Type -TypeDefinition  @"
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+public static class TrustEverything
+{
+    private static bool ValidationCallback(object sender, X509Certificate certificate, X509Chain chain,
+    SslPolicyErrors sslPolicyErrors) { return true; }
+    public static void SetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = ValidationCallback; }
+    public static void UnsetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = null; } } 
 "@
+        }
+        [TrustEverything]::SetCallback()
+    }
+    function Enable-SslVerification {
+        if (([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
+            [TrustEverything]::UnsetCallback()
+        }
+    }
+    Disable-SslVerification   
+}
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocoltype]::Tls12 
 
-$tenantList = if ($script:tenantsCSVFile -and (Test-Path $script:tenantsCSVFile)) {
-    get-content -Raw -Path $script:tenantsCSVFile | ConvertFrom-Csv -Delimiter ','
-} else {
-    $completeTenantList_text | ConvertFrom-Csv -Delimiter ','
+# Construct the headers for this API request 
+$headers = @{
+    Authorization  = "Api-Token $script:clustertoken";
+    Accept         = "application/json; charset=utf-8";
+    "Content-Type" = "application/json; charset=utf-8"
 }
 
-
-if ($tenantList.length -eq 0) {
-    write-host "The internal tenantlist CSV string is empty. No tenants to run against." -ForegroundColor Red
-    exit
+function confirm-supportedClusterVersion ($minimumVersion = 194, $logmsg = '') {
+    # Environment version check - cancel out if too old 
+    $uri = "$script:dtcluster/api/v1.0/onpremise/cluster"
+    Write-Host -ForegroundColor cyan -Object "Cluster Version Check$logmsg`: GET $uri"
+    $res = Invoke-RestMethod -Method GET -Headers $headers -Uri $uri 
+    $envVersion = $res[0].buildVersion -split '\.'
+    if ($envVersion -and ([int]$envVersion[0]) -ne 1 -and ([int]$envVersion[1]) -lt $minimumVersion) {
+        write-host "Failed Environment version check - Expected: > 1.176 - Got: $($res.version)"
+        exit
+    }
 }
 
-if ($script:list) {
-    Write-host "Internal tenantlist contains the following:"
-    $tenantList | 
-    ForEach-Object { $i } { $_ | Add-Member -Type NoteProperty -Name 'Index' -Value ($i++) -PassThru } | 
-    Select-Object -property index, name, environment | 
-    Format-table -AutoSize
-    exit
+function confirm-requiredTokenPerms ($token, $requirePerms, $logmsg = '') {
+    # Token has required Perms Check - cancel out if it doesn't have what's required
+    $uri = "$baseURL/tokens/lookup"
+    Write-Host -ForegroundColor cyan -Object "Token Permissions Check$logmsg`: POST $uri"
+    $res = Invoke-RestMethod -Method POST -Headers $headers -Uri $uri -body "{ `"token`": `"$script:clustertoken`"}"
+    if (($requirePerms | Where-Object { $_ -notin $res.scopes }).count) {
+        write-host "Failed Token Permission check. Token requires: $($requirePerms -join ',')"
+        write-host "Token provided only had: $($res.scopes -join ',')"
+        exit
+    }
 }
 
-if ( -not $script:ScriptBlock) {
+if (!$noCheckCompatibility) {
+    <#
+        Determine what type environment we have? This script will only work on tenants 
+        
+        SaaS tenant = https://*.live.dynatrace.com
+        Managed tenant = https://*/e/UUID
+        Managed Cluster = https://*
+    #>
+    $envType = 'cluster'
+    if ($script:dtcluster -like "*.live.dynatrace.com") {
+        $envType = 'env'
+    }
+    elseif ($script:dtcluster -like "http*://*/e/*") {
+        $envType = 'env'
+    }
+
+    # Script won't work on a tenant
+    if ($envType -ne 'cluster') {
+        write-error "'$script:dtcluster' looks like an invalid URL (only Clusters are supported by this script)"
+        return
+    }
+    
+    confirm-supportedClusterVersion 184
+    confirm-requiredTokenPerms $script:clustertoken $script:tokenPermissionRequirements
+}
+
+<#########################
+# Stop of scaffold block #
+#########################>
+
+if ( -not $script:ScriptBlock -and -not $script:list) {
     write-host -ForegroundColor Red "No Script block was provided - nothing happened"
     exit
 }
 
-$_tp = 7856413
+# Assemble the list of tenants we're working on
+$baseURL = "$script:dtcluster/api/cluster/v2"
+$uri = "$baseURL/environments"
+
+$script:tenantList = @()
+# Did they specify an id?
+if ( $script:tenantid ) {
+    $uri += "/$script:tenantid"
+    write-host -ForegroundColor Cyan "Fetching environment based on provided ID: GET $uri"
+    $res = Invoke-RestMethod -Method GET -Headers $headers -Uri $uri
+
+    $script:tenantList = @($res)
+}
+# If they gave us something more generic
+else {
+    $uri += "?pageSize=500"
+    $filtersPrefix = "&filter="
+    $filters = @()
+    $filters += if ($script:filter) { 'name("' + $script:filter + '")' }
+    $filters += if ($script:tag) { 'tag("' + ($script:tag -join '","') + '")' }
+    $filters += 'state(ENABLED)'
+    $uri += ($filtersPrefix + ($filters -join ','))
+    
+    write-host -ForegroundColor Cyan "Fetching environments based on provided options: GET $uri"
+    $res = Invoke-RestMethod -Method GET -Headers $headers -Uri $uri
+
+    if ($res.totalCount -lt 1) {
+        write-host "Could not find any tenants matching your request" -ForegroundColor Red
+        exit
+    }
+
+    $script:tenantList = $res.environments
+}
+
+if ($script:list) {
+    Write-host "Internal tenantlist contains the following:"
+    return $script:tenantList
+}
+
+try {
+    # Create a new EnvironmentTokenManagement token
+    $body = @"
+{
+    "name": "_Transienttoken_ created by run-clusterwide.ps1",
+    "expiresIn": {"value": 1,"unit": "HOURS"},
+    "scopes": ["EnvironmentTokenManagement"]
+}
+"@
+    $uri = "$baseURL/tokens"
+    write-host -ForegroundColor Cyan "Creating temporary temporary management token: POST $uri"
+    $res = Invoke-RestMethod -Method POST -Headers $headers -Uri $uri -Body $body -ErrorAction stop
+    $script:ETMToken = $res.token
+}
+catch {
+    return Write-Error "Unable to create a temporary token with the scope 'EnvironmentTokenManagement'`n$_"
+}
+
+$ETMHeader = @{
+    'Authorization' = "Api-Token $script:ETMToken";
+    'Accept'        = "application/json; charset=utf-8";
+    "Content-Type"  = "application/json; charset=utf-8"
+}
+$ETMBody = @"
+{
+    "name": "!!Temporary Token!! - Created for run-clusterwide.ps1 - $(get-date -UFormat "%Y%m%d.%H%M")",
+    "expiresIn": {"value": 3600,"unit": "SECONDS"}
+}
+"@
+
+# Create a short-term token for each of the target environments
+$tempEnvTokens = $tenantList | ForEach-Object { $o = @() } {
+    # create a new token in the environment
+    $uri = "$baseURL/environments/$($_.id)/tokenManagementToken"
+    write-host -ForegroundColor Cyan "Creating TokenManagementToken: POST $uri"
+    $TokenManagementRes = Invoke-RestMethod -Method POST -Headers $ETMHeader -Uri $uri -Body $ETMBody
+
+    $rooturi = "$script:dtcluster/e/$($_.id)"
+    $uri = "$rooturi/api/v1/tokens"
+    $envHeader = @{
+        'Authorization' = "Api-Token $($TokenManagementRes.token)";
+        'Accept'        = "application/json; charset=utf-8";
+        "Content-Type"  = "application/json; charset=utf-8"
+    }
+    $body = @"
+    {
+        "name": "Temporary ScriptToken",
+        "expiresIn": {"value": 120,"unit": "SECONDS"},
+        "scopes": $($envtokenPerms | ConvertTo-Json)
+    }
+"@
+    write-host -ForegroundColor Cyan "Creating Token for execution against $($_.name): POST $uri"
+    $envRes = Invoke-RestMethod -Method POST -Headers $envHeader -Uri $uri -Body $body
+
+    $_o = New-Object -TypeName psobject 
+    $_o | Add-Member -Type NoteProperty -Name env -Value $_.id
+    $_o | Add-Member -Type NoteProperty -Name uri -Value $rooturi
+    $_o | Add-Member -Type NoteProperty -Name scriptToken -Value $envRes.token
+    $_o | Add-Member -Type NoteProperty -Name tokenManagementToken -Value $TokenManagementRes.token
+
+    $o += $_o
+    
+} { $o }
+
+$_tp = 710563
 <#
     x = new job checking loop
     | = job completed
     . = job started
+    Manage-Job taken from https://github.com/cavejay/strippy
 #>
 function Manage-Job ([System.Collections.Queue] $jobQ, [int] $MaxJobs = 8, $delay = 200) {
     write-host "[START] Managing Job Execution"
@@ -126,7 +351,7 @@ function Manage-Job ([System.Collections.Queue] $jobQ, [int] $MaxJobs = 8, $dela
     $totalJobs = $jobQ.count
     $ProgressInterval = 100 / $totalJobs
     # While there are still jobs to deploy or there are jobs still running
-   While ($jobQ.Count -gt 0 -or $(get-job -State "Running").count -gt 0) {
+    While ($jobQ.Count -gt 0 -or $(get-job -State "Running").count -gt 0) {
         $JobsRunning = $(Get-Job -State 'Running').count
         write-host 'x' -NoNewline
 
@@ -186,23 +411,15 @@ function Manage-Job ([System.Collections.Queue] $jobQ, [int] $MaxJobs = 8, $dela
 }
 
 #filter tenant list if necessary
-if ( $tenant ) {
-    $tenantList = $tenantList | Where-Object -Property name -eq -Value $tenant
-}
-elseif ( $tenantIndex ) {
-    # index is valid
-    If (0..($tenantList.length - 1) -notcontains $tenantIndex) {
-        Write-Error "Invalid $tenantIndex."
-        Exit # return early
-    }
-    $tenantList = $tenantList[$tenantIndex]
+if ( $script:tenantId ) {
+    $tenantList = $tenantList | Where-Object -Property name -eq -Value $script:tenantId
 }
 
 $jobQueue = New-Object System.Collections.Queue
 foreach ($t in $tenantList) {
     $prefilledVariables = [scriptblock]::Create(@"
-`$env:dtenv = `$_env = '$($t.environment)'; 
-`$env:dttoken = `$_token = '$($t.token)';
+`$env:dtenv = `$_env = '$($tempEnvTokens | Where-Object -Property env -EQ -Value $t.id | Select-Object -ExpandProperty uri)';
+`$env:dttoken = `$_token = '$($tempEnvTokens | Where-Object -Property env -EQ -Value $t.id | Select-Object -ExpandProperty scripttoken)';
 `$env:dtenvname = `$_envname = '$($t.Name)';
 
 # CD to the current directory
@@ -212,6 +429,45 @@ Set-Location $(Get-Location)
 }
 
 Manage-Job $jobQueue $script:maxJobs $script:JobDelay
+
+# Clean up the tokens we made everywhere
+
+$ETMHeader = @{
+    'Authorization' = "Api-Token $script:ETMToken";
+    'Accept'        = "application/json; charset=utf-8";
+    "Content-Type"  = "application/json; charset=utf-8"
+}
+# Create a short-term token for each of the target environments
+$tempEnvTokens | ForEach-Object {
+    
+    # Clean up the script Token
+    $envHeader = @{
+        'Authorization' = "Api-Token $($_.tokenManagementToken)";
+        'Accept'        = "application/json; charset=utf-8";
+        "Content-Type"  = "application/json; charset=utf-8"
+    }
+
+    $uri = "$($_.uri)/api/v1/tokens/lookup"
+    $body = "{`"token`": `"$($_.scriptToken)`"}"
+    write-host -ForegroundColor Cyan "Get ID of script token: POST $uri"
+    $scriptTokenDetails = Invoke-RestMethod -Method POST -Headers $envHeader -Uri $uri -Body $body
+
+    $uri = "$($_.uri)/api/v1/tokens/$($scriptTokenDetails.id)"
+    write-host -ForegroundColor cyan "Delete Token from $($_.env): DELETE $uri"
+    $res = Invoke-RestMethod -Method Delete -Headers $envHeader -Uri $uri
+    
+}
+
+# Clean up the global token
+$uri = "$script:dtcluster/api/cluster/v2/tokens/lookup"
+$body = "{`"token`": `"$($script:ETMToken)`"}"
+write-host -ForegroundColor Cyan "Get ID of Environment Token Management Token: POST $uri"
+$ETMtokenDetails = Invoke-RestMethod -Method POST -Headers $headers -Uri $uri -Body $body
+
+$uri = "$script:dtcluster/api/cluster/v2/tokens/$($ETMtokenDetails.id)"
+write-host -ForegroundColor cyan "Delete Token from cluster: DELETE $uri"
+$res = Invoke-RestMethod -Method Delete -Headers $headers -Uri $uri
+Write-Host $res
 
 # Collect the output from each of the jobs
 $jobs = Get-Job -State Completed
