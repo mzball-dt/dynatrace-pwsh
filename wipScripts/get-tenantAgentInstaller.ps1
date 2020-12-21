6826b7a02a55c19ccb6d615cb72928e6a9ad4b83
Adrian Chen <adrian.chen@dynatrace.com>
2020-12-21 15:23:27 +1100

    Added WIP scripts

---
+++ wipScripts/get-tenantAgentInstaller.ps1
@@ -0,0 +1,180 @@
+<#
+    .Synopsis
+        Basis for creating API scripts
+    .Description
+        This script forms the basis (scaffold) for scripts to perform API calls
+    .Notes
+        Author: <Author Name>
+        Version: 1.0.0 - <date>
+        ChangeLog
+            1.0.0 
+                MVP - Things work
+            
+    .Example
+        <PLACEHOLDER> - insert sample usage here
+#>
+
+<#
+###########################
+# Start of scaffold block #
+###########################
+#>
+PARAM (
+    # The cluster or tenant the HU report should be fetched from
+    [Parameter()][ValidateNotNullOrEmpty()] $dtenv = $env:dtenv,
+    # Token must have Smartscape Read access for a tenant
+    [Alias('dttoken')][ValidateNotNullOrEmpty()][string] $token = $env:dttoken,
+
+    <##################################
+    # Start of Script-specific params #
+    ##################################>
+
+
+
+    <#################################
+    # Stop of Script-specific params #
+    #################################>
+
+    # Prints Help output
+    [Alias('h')][switch] $help,
+    # use this switch to tell this script to not check token or cluster viability
+    [switch] $noCheckCompatibility,
+    # use this switch to tell powershell to ignore ssl concerns
+    [switch] $noCheckCertificate,
+
+    # DO NOT USE - This is set by Script Author
+    [String[]]$script:tokenPermissionRequirements = @('DataExport')
+)
+
+# Help flag checks
+if ($h -or $help) {
+    Get-Help $script:MyInvocation.MyCommand.Path -Detailed
+    exit 0
+}
+
+# Ensure that dtenv and token are both populated
+if (!$script:dtenv) {
+    return Write-Error "dtenv was not populated - unable to continue"
+}
+elseif (!$script:token) {
+    return Write-Error "token/dttoken was not populated - unable to continue"
+}
+
+# Try to 'fix' a missing https:// in the env
+if ($script:dtenv -notlike "https://*" -and $script:dtenv -notlike "http://*") {
+    Write-Host -ForegroundColor DarkYellow -Object "WARN: Environment URI was missing 'httpx://' prefix"
+    $script:dtenv = "https://$script:dtenv"
+    Write-host -ForegroundColor Cyan "New environment URL: $script:dtenv"
+}
+
+# Try to 'fix' a trailing '/'
+if ($script:dtenv[$script:dtenv.Length - 1] -eq '/') { 
+    $script:dtenv = $script:dtenv.Substring(0, $script:dtenv.Length - 1) 
+    write-host -ForegroundColor DarkYellow -Object "WARNING: Removed trailing '/' from dtenv input"
+}
+
+$baseURL = "$script:dtenv/api/v1"
+
+# Setup Network settings to work from less new setups
+if ($nocheckcertificate) {
+    # SSL and other compatability settings
+    function Disable-SslVerification {
+        if (-not ([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
+            Add-Type -TypeDefinition  @"
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+public static class TrustEverything
+{
+    private static bool ValidationCallback(object sender, X509Certificate certificate, X509Chain chain,
+    SslPolicyErrors sslPolicyErrors) { return true; }
+    public static void SetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = ValidationCallback; }
+    public static void UnsetCallback() { System.Net.ServicePointManager.ServerCertificateValidationCallback = null; } } 
+"@
+        }
+        [TrustEverything]::SetCallback()
+    }
+    function Enable-SslVerification {
+        if (([System.Management.Automation.PSTypeName]"TrustEverything").Type) {
+            [TrustEverything]::UnsetCallback()
+        }
+    }
+    Disable-SslVerification   
+}
+
+## Sets TLS to 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocoltype]::Tls12 
+
+# Construct the headers for this API request 
+$headers = @{
+    Authorization  = "Api-Token $script:token";
+    Accept         = "application/json; charset=utf-8";
+    "Content-Type" = "application/json; charset=utf-8"
+}
+
+function confirm-supportedClusterVersion ($minimumVersion = 176, $logmsg = '') {
+    # Environment version check - cancel out if too old 
+    $uri = "$baseURL/config/clusterversion"
+    Write-Host -ForegroundColor cyan -Object "Cluster Version Check$logmsg`: GET $uri"
+    $res = Invoke-RestMethod -Method GET -Headers $headers -Uri $uri 
+    $envVersion = $res.version -split '\.'
+    if ($envVersion -and (([int]$envVersion[0]) -ne 1 -or ([int]$envVersion[1]) -lt $minimumVersion)) {
+        Write-Error "Failed Environment version check - Expected: > 1.$minimumVersion - Got: $($res.version)"
+        exit
+    }
+}
+
+function confirm-requiredTokenPerms ($token, $requirePerms, $logmsg = '') {
+    # Token has required Perms Check - cancel out if it doesn't have what's required
+    $uri = "$baseURL/tokens/lookup"
+    $headers = @{
+        Authorization  = "Api-Token $token";
+        Accept         = "application/json; charset=utf-8";
+        "Content-Type" = "application/json; charset=utf-8"
+    }
+    Write-Host -ForegroundColor cyan -Object "Token Permissions Check$logmsg`: POST $uri"
+    $res = Invoke-RestMethod -Method POST -Headers $headers -Uri $uri -body "{ `"token`": `"$token`"}"
+    if (($requirePerms | Where-Object { $_ -notin $res.scopes }).count) {
+        Write-Error "Failed Token Permission check. Token requires: $($requirePerms -join ', ')"
+        write-host "Token provided only had: $($res.scopes -join ', ')"
+        exit
+    }
+}
+
+if (!$noCheckCompatibility) {
+    <#
+        Determine what type environment we have? This script will only work on tenants 
+        
+        SaaS tenant = https://*.live.dynatrace.com
+        Managed tenant = https://*/e/UUID
+        Managed Cluster = https://*
+    #>
+    $envType = 'cluster'
+    if ($script:dtenv -like "*.live.dynatrace.com") {
+        $envType = 'env'
+    }
+    elseif ($script:dtenv -like "http*://*/e/*") {
+        $envType = 'env'
+    }
+
+    # Script won't work on a cluster
+    if ($envType -eq 'cluster') {
+        write-error "'$script:dtenv' looks like an invalid URL (and Clusters are not supported by this script)"
+        exit
+    }
+    
+    # check that other requirements are met
+    confirm-supportedClusterVersion 192
+    confirm-requiredTokenPerms $script:token $script:tokenPermissionRequirements
+}
+
+function convertTo-jsDate($date) {
+    return [Math]::Floor(1000 * (Get-Date ($date) -UFormat %s))
+}
+
+$baseURL = "$script:dtenv/api/v1"
+
+<#
+###########################
+# End of scaffold block #
+###########################
+#>
