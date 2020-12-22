763ec7857a2c0b8d7bd728a79e8c9a8bb13db318
Adrian Chen <adrian.chen@dynatrace.com>
2020-12-22 14:06:08 +1100

    Updated Comments. Retrieved the cluster health.

--- wipScripts/get-clusterHealth.ps1
+++ wipScripts/get-clusterHealth.ps1
@@ -2,10 +2,10 @@
     .Synopsis
         A Basic Health Check script to quickly identify cluster / activeGate issues
     .Description
-        This script forms the basis (scaffold) for scripts to perform API calls
+        This script performs a health check and provideds output of the results. 
     .Notes
         Author: Adrian Chen
-        Version: 1.0.0 - <date>
+        Version: 1.0.0 - <date> ## No date until 1.0.0 is available ##
         ChangeLog
             1.0.0 
                 MVP - Things work
@@ -20,9 +20,9 @@
 ###########################>
 
 PARAM (
-    # The cluster or tenant the Dasboard will be placed in
+    # The health check target cluster 
     [Parameter()][ValidateNotNullOrEmpty()] $dtcluster = $env:dtcluster,
-    # Token for the destination tenant w/ DataExport and WriteConfig perms
+    # Token for target cluster w/ ServiceProviderAPI perms
     [Alias('dtclustertoken')][ValidateNotNullOrEmpty()][string] $clustertoken = $env:dtclustertoken,
 
     <##################################
@@ -52,7 +52,7 @@ if ($h -or $help) {
 
 # Ensure that dtcluster and token are both populated
 if (!$script:dtcluster) {
-    return Write-Error "dtcluster was not populated - unable to continue"
+    return Write-Error "Cluster URL was not populated - unable to continue"
 }
 elseif (!$script:clustertoken) {
     return Write-Error "clustertoken/dtclustertoken was not populated - unable to continue"
@@ -108,12 +108,12 @@ $headers = @{
 }
 
 function confirm-supportedClusterVersion ($minimumVersion = 194, $logmsg = '') {
-    # Environment version check - cancel out if too old 
+    # Cluster version check - cancel out if too old 
     $uri = "$script:dtcluster/api/v1.0/onpremise/cluster"
     Write-Host -ForegroundColor cyan -Object "Cluster Version Check$logmsg`: GET $uri"
     $res = Invoke-RestMethod -Method GET -Headers $headers -Uri $uri 
-    $envVersion = $res[0].buildVersion -split '\.'
-    if ($envVersion -and ([int]$envVersion[0]) -ne 1 -and ([int]$envVersion[1]) -lt $minimumVersion) {
+    $clusterVersion = $res[0].buildVersion -split '\.'
+    if ($clusterVersion -and ([int]$clusterVersion[0]) -ne 1 -and ([int]$clusterVersion[1]) -lt $minimumVersion) {
         write-host "Failed Environment version check - Expected: > 1.176 - Got: $($res.version)"
         exit
     }
@@ -161,34 +161,125 @@ if (!$noCheckCompatibility) {
 # Stop of scaffold block #
 #########################>
 
-## Redefine
+$sslErrorMsg = "Could not establish trust relationship"
+
+$nodeInfo = @()
+
+## Redefine baseURL
 ## Shorten baseURL as a mix of v1 and v2 are used 
 $baseURL = "$script:dtcluster/api"
 
-## Retrieve cluster node information
-$res = Invoke-webRequest -uri "$baseURL/v1.0/onpremise/cluster/configuration" -Headers $headers
+## Get Config from current cluster
 
-$nodeList
+Write-host "[Info]: Retrieving current cluster configuration information."
+
+try {
+    $res = Invoke-RestMethod -uri "$baseURL/v1.0/onpremise/cluster/configuration" -Headers $headers -ErrorAction Continue
+}
+catch {
+    if ($_ -ilike "*$sslErrorMsg*") {
+        Write-Host "Error: SSL Error, try re-run the script with -nocheckcertificate." -ForegroundColor Red
+        Write-Host "Details: $($_.Exception)"
+    }
+    else {
+        Write-Host "Error: $_" -ForegroundColor Red
+        Write-host "[Error]: Failed to retrieve current cluster configuration information."
+        Write-host "`t Cluster : $($dtcluster)"
+    }
+}
+
+## Get the state of each node from the node
+foreach ($node in $res.clusterNodes) {
+    $_currentNode = New-Object PSObject
+    
+    ## Set node details
+
+    ## TODO
+    ## There is a better way of assigning the values, use the other method in next iterations
+    $_currentNode | Add-Member -MemberType NoteProperty -Name "webUI" -Value $node.webUI
+    $_currentNode | Add-Member -MemberType NoteProperty -Name "agent" -Value $node.agent
+    $_currentNode | Add-Member -MemberType NoteProperty -Name "datacenter" -Value $node.datacenter
+    $_currentNode | Add-Member -MemberType NoteProperty -Name "kubernetesRole" -Value $node.kubernetesRole
+    $_currentNode | Add-Member -MemberType NoteProperty -Name "Id" -Value $node.Id
+    $_currentNode | Add-Member -MemberType NoteProperty -Name "ipAddress" -Value $node.ipAddress
+
+    ## Cluster Node Status Check 
+    ## Also find out which node is the master definitively
+    try {
+        $res = $null
+        $res = Invoke-RestMethod -uri "https://$($node.ipAddress):8021/api/v1.0/onpremise/nodeManagement/nodeServerStatus" -Headers $headers
+        $_currentNode | Add-Member -MemberType NoteProperty -Name "master" -Value $res.master
+        $_currentNode | Add-Member -MemberType NoteProperty -Name "operationState" -Value $res.operationState
+    }
+    catch {
+        if ($_ -ilike "*$sslErrorMsg*") {
+            Write-Host "Error: SSL Error, try re-run the script with -nocheckcertificate." -ForegroundColor Red
+            Write-Host "Details: $($_.Exception)"
+        }
+        else {
+            Write-Host "Error: $_" -ForegroundColor Red
+        }
+    }
+    $nodeInfo += $_currentNode
+}
+
+## Test only Output 
+#$nodeInfo | Format-Table
+## TODO 
+## Evaluation for a healthy node - webUI = true, agent = true, operationState = running
 
 ## Get firewall rules 
 ## Mismatch would indicate a node is blocked
-$res = Invoke-webRequest -uri "$baseURL/v1.0/onpremise/firewallManagement/clusterNodes" -Headers $headers
-$res
-## Cluster Node Status Check 
-## Also find out which node is the master definitively
-$res = Invoke-webRequest -uri "<NODE_DIRECT_CNAME_OR_IP>/api/v1.0/onpremise/nodeManagement/nodeServerStatus" -Headers $headers
-$res
-$nodeHealth
+try {
+    $res = $null
+    $res = Invoke-RestMethod -uri "$baseURL/v1.0/onpremise/firewallManagement/clusterNodes" -Headers $headers -ErrorAction Continue
+}
+catch {
+    if ($_ -ilike "*$sslErrorMsg*") {
+        Write-Host "Error: SSL Error, try re-run the script with -nocheckcertificate." -ForegroundColor Red
+        Write-Host "Details: $($_.Exception)"
+    }
+    else {
+        Write-Host "Error: $_" -ForegroundColor Red
+    }
+}
+
+$res | Format-Table
 
 ## Synthetics Nodes (Synthetic enabled Cluster ActiveGates' health)
-$res = Invoke-webRequest -uri "$baseURL/cluster/v2/synthetic/nodes" -Headers $headers
-$res
+try {
+    $res = $null
+    $res = Invoke-RestMethod -uri "$baseURL/cluster/v2/synthetic/nodes" -Headers $headers -ErrorAction Continue
+}
+catch {
+    if ($_ -ilike "*$sslErrorMsg*") {
+        Write-Host "Error: SSL Error, try re-run the script with -nocheckcertificate." -ForegroundColor Red
+        Write-Host "Details: $($_.Exception)"
+    }
+    else {
+        Write-Host "Error: $_" -ForegroundColor Red
+    }
+}
 
+$res | Format-Table
 $activeGate
 
 ## Cluster ActiveGates' health - Not Offline = Healthy
-$res = Invoke-webRequest -uri "$baseURL/cluster/v2/activeGates" -Headers $headers
-$res
+try {
+    $res = $null
+    $res = Invoke-RestMethod -uri "$baseURL/cluster/v2/activeGates" -Headers $headers -ErrorAction Continue
+}
+catch {
+    if ($_ -ilike "*$sslErrorMsg*") {
+        Write-Host "Error: SSL Error, try re-run the script with -nocheckcertificate." -ForegroundColor Red
+        Write-Host "Details: $($_.Exception)"
+    }
+    else {
+        Write-Host "Error: $_" -ForegroundColor Red
+    }
+}
+
+$res | Format-Table
 
 ## AG Port Check
 try {
